b821fa7971b6602bedf4ff032dbe737451750040
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-21 13:25:26 +0900

    ログアウト機能作成

--- src/Login.tsx
+++ src/Login.tsx
@@ -9,9 +9,10 @@ const Login: React.FC = (props: any) => {
   const [password, setPassword] = useState("");
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    const unSub = auth.onAuthStateChanged((user) => {
       user && props.history.push("/login");
     });
+    return () => unSub();
   }, [props.history]);
 
   return (
